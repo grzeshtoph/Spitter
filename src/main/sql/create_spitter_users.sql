0e40397875391a41af44d31a213c3af4bce1c3d9
gregs <greg.smorag@gmail.com>
2013-09-17 21:56:26 +0100

    Unitils init

---
+++ src/main/sql/create_spitter_users.sql
@@ -0,0 +1,6 @@
+create sequence user_id_seq;
+create table spitter_users ( 
+	user_id int not null default nextval('user_id_seq'),
+	username varchar(50) not null,
+	password varchar(50) not null,
+	full_name varchar(100) not null);
